5509a3c9a9a5719021748b13353f10b4ba1709ec
victor-chukwu <okenwa1993@gmail.com>
2021-12-16 18:12:13 +0100

    fixing linters

--- english_to_morse_code.rb
+++ english_to_morse_code.rb
@@ -5,26 +5,23 @@ def decode_char(alphabet)
     '--' => 'M', '-.' => 'N', '---' => 'O', '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R',
     '...' => 'S', '-' => 'T', '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X',
     '-.--' => 'Y', '--..' => 'Z'
-  }
-  
+  } 
   morse[alphabet]
 end
-
   def decode_word(alphabet)
-    word_split = alphabet.split(" ")
-    decoded_word = ""
+    word_split = alphabet.split(' ')
+    decoded_word = ''
     word_split.each do |letter|
       decoded_word += decode_char(letter)
     end
     decoded_word
   end
-  
 
   def decode(str)
-    sentence_split = str.split("   ")
-    full_sentence_decoded = ""
+    sentence_split = str.split(' ')
+    full_sentence_decoded = ''
     sentence_split.each do |word|
-      full_sentence_decoded += decode_word(word) + " "
+      full_sentence_decoded += decode_word(word) + ''
     end
     full_sentence_decoded.strip
   end
