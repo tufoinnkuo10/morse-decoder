9a9de63aeacabb13fbc5f56b6ecac1d95a4ede45
tufoinnkuo10 <tufoinnkuo10@gmail.com>
2021-12-16 18:41:26 +0100

    rubocop

--- english_to_morse_code.rb
+++ english_to_morse_code.rb
@@ -5,25 +5,26 @@ def decode_char(alphabet)
     '--' => 'M', '-.' => 'N', '---' => 'O', '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R',
     '...' => 'S', '-' => 'T', '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X',
     '-.--' => 'Y', '--..' => 'Z'
-  } 
+  }
   morse[alphabet]
 end
-  def decode_word(alphabet)
-    word_split = alphabet.split(' ')
-    decoded_word = ''
-    word_split.each do |letter|
-      decoded_word += decode_char(letter)
-    end
-    decoded_word
-  end
 
-  def decode(str)
-    sentence_split = str.split(' ')
-    full_sentence_decoded = ''
-    sentence_split.each do |word|
-      full_sentence_decoded += decode_word(word) + ''
-    end
-    full_sentence_decoded.strip
+def decode_word(alphabet)
+  word_split = alphabet.split(' ')
+  decoded_word = ''
+  word_split.each do |letter|
+    decoded_word += decode_char(letter)
   end
-  
-  print decode('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
+  decoded_word
+end
+
+def decode(str)
+  sentence_split = str.split(' ')
+  full_sentence_decoded = ''
+  sentence_split.each do |word|
+    full_sentence_decoded += decode_word(word) + ''
+  end
+  full_sentence_decoded.strip
+end
+
+print decode('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
